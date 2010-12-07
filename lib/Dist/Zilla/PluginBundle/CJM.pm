acd05c91d1d206e379778b00676ef135122e2780
Christopher J. Madsen <perl@cjmweb.net>
2010-12-07 13:25:17 -0600

    Require PodLoom 3.01 & MatchManifest 4.00

--- lib/Dist/Zilla/PluginBundle/CJM.pm
+++ lib/Dist/Zilla/PluginBundle/CJM.pm
@@ -17,7 +17,7 @@ package Dist::Zilla::PluginBundle::CJM;
 # ABSTRACT: Build a distribution like CJM
 #---------------------------------------------------------------------
 
-our $VERSION = '0.10';
+our $VERSION = '4.00';
 # This file is part of {{$dist}} {{$dist_version}} ({{$date}})
 
 use Moose;
