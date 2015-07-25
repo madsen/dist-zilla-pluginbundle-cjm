402c5c4d9bff0a6c74610aec62ef1f77e7feeabc
Christopher J. Madsen <perl@cjmweb.net>
2015-07-25 10:32:11 -0500

    Update prereqs to better support POD in scripts
      Require PodLoom version 5.001 (uses filename instead of 'main')
      Require TemplateCJM version 4.23 (adds $filename & $pathname)
      Bump version to 4.34

--- lib/Dist/Zilla/PluginBundle/Author/CJM.pm
+++ lib/Dist/Zilla/PluginBundle/Author/CJM.pm
@@ -17,7 +17,7 @@ package Dist::Zilla::PluginBundle::Author::CJM;
 # ABSTRACT: Build a distribution like CJM
 #---------------------------------------------------------------------
 
-our $VERSION = '4.33';
+our $VERSION = '4.34';
 # This file is part of {{$dist}} {{$dist_version}} ({{$date}})
 
 use Moose;
