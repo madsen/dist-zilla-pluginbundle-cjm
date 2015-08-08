e9627d78c67e7cba701d622792e8f57ffb9d7baf
Christopher J. Madsen <perl@cjmweb.net>
2015-08-08 11:26:07 -0500

    Add 10-build.t
      Bump version to 4.35

--- lib/Dist/Zilla/PluginBundle/Author/CJM.pm
+++ lib/Dist/Zilla/PluginBundle/Author/CJM.pm
@@ -17,7 +17,7 @@ package Dist::Zilla::PluginBundle::Author::CJM;
 # ABSTRACT: Build a distribution like CJM
 #---------------------------------------------------------------------
 
-our $VERSION = '4.34';
+our $VERSION = '4.35';
 # This file is part of {{$dist}} {{$dist_version}} ({{$date}})
 
 use Moose;
